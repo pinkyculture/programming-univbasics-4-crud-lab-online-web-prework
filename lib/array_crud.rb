e8d047f6e72befb89e1b33c8426f3c3f0ca53608
interactive-cache-8375 <randomblue1@yahoo.com>
2019-10-06 05:44:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ end
 
 def remove_element_from_start_of_array(backstreet_boys)
   
-  "ajmclean" = backstreet_boys.shift 
+  aj_mclean = backstreet_boys.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
